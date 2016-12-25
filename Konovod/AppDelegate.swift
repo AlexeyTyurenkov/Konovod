395cbdbcda213284478673818f56db55f123edb5
Oleksii Tiurenkov <alterego4@gmail.com>
2016-12-25 11:27:03 +0200

    Initial commit

---
+++ Konovod/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Konovod
+//
+//  Created by Aleksey Tyurenkov on 12/25/16.
+//  Copyright © 2016 com.ovt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Konovod/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Konovod
+//
+//  Created by Aleksey Tyurenkov on 12/25/16.
+//  Copyright © 2016 com.ovt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
